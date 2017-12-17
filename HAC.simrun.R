50e8b3425b55b09944cd969effbc4615426991e2
Jarrett Phillips <jphill01@uoguelph.ca>
2017-12-16 19:21:53 -0500

    Add files via upload

--- HAC.simrun.R
+++ HAC.simrun.R
@@ -3,11 +3,11 @@
 ### Set parameters ###
 
 K <- 1  # number of equally-sized (sub)populations
-N <- 50 # total number of sampled individuals
-Hstar <- 10 # total number of haplotypes
+N <- 240 # total number of sampled individuals
+Hstar <- 15 # total number of haplotypes
 probs <- c(220/N, rep(3/N, 2), rep(2/N, 2), rep(1/N, 10)) # haplotype frequency distribution
 perms <- 10000 # number of permutations
-p <- 0.90 # proportion of haplotypes to recover 
+p <- 0.95 # proportion of haplotypes to recover 
 plot.out <- TRUE # plot curve and barplot
 
 
