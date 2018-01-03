ba7b1c3a228a68122a5628dbc8a72533f797f245
Jarrett Phillips <jphill01@uoguelph.ca>
2018-01-03 01:01:11 -0500

    Add files via upload

--- HAC.simrep.R
+++ HAC.simrep.R
@@ -4,7 +4,7 @@
 
 HAC.simrep <- function() {
 	while (R < p) {
-		HAC.sim(N = ceiling(Nstar), Hstar = Hstar, probs = probs, K = K, perms = perms, p = p, plot.out = plot.out)
+		HAC.sim(N = ceiling(Nstar), Hstar = Hstar, probs = probs, K = K, m = m, perms = perms, p = p, plot.out = plot.out)
 	}
 }
 
