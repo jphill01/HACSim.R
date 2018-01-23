6739dedde07a5a109328fd80948a66d402a31e4f
Jarrett Phillips <jphill01@uoguelph.ca>
2018-01-22 20:21:51 -0500

    Add files via upload

--- migrate.cpp
+++ migrate.cpp
@@ -6,25 +6,26 @@
 using namespace Rcpp;
 
 // [[Rcpp::export]]
-arma::Cube<int> migrate(const arma::Cube<int> pop) {
-int i, j, k, K, m, perms, tmp;
-std::string model;
-IntegerVector ind1, ind2;
+arma::Cube<int> migrate(arma::Cube<int> pop) {
+    String model;
+    int i, j, k, K, perms, tmp;
+    double m = 0;
+    IntegerVector ind1, ind2;
     
     if (m != 0) {
         if (model == "Step") {
             for (i = 0; i < K; i++) {
                 for (j = 0; j < K; j++) {
                     for (k = 1; k < (K - 1); k++) {
-                        ind1 = sample(perms, std::ceil(perms * m / 2), false);
-                        ind2 = sample(perms, std::ceil(perms * m / 2), false);
-                        tmp = pop[ind1, RcppArmadillo::sample(k)];
-                        pop[ind1, RcppArmadillo::sample(k)] = pop[ind2, RcppArmadillo::sample(k)];
-                        pop[ind2, RcppArmadillo::sample(k)] = tmp;
-                        return pop;
+                        ind1 = sample(perms, ceil(perms * m / 2), false);
+                        ind2 = sample(perms, ceil(perms * m / 2), false);
+                        tmp = pop[ind1, RcppArmadillo::sample(k, k, true)];
+                        pop[ind1, RcppArmadillo::sample(k, k, true)] = pop[ind2, RcppArmadillo::sample(k, k, true)];
+                        pop[ind2, RcppArmadillo::sample(k, k, true)] = tmp;
+                        }
                     }
                 }
             }
         }
-    }
+    return pop;
 }
