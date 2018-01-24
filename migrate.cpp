5d9d283f1d13881a51b55cb4c0030ac49760cafe
Jarrett Phillips <jphill01@uoguelph.ca>
2018-01-24 12:20:46 -0500

    Add files via upload

--- migrate.cpp
+++ migrate.cpp
@@ -8,24 +8,20 @@ using namespace Rcpp;
 arma::Cube<int> pop;
 double m;
 const String model;
-int i, j, k, K, perms, tmp;
+int i, ind, K, perms;
+double tmp;
 
 // [[Rcpp::export]]
 arma::Cube<int> migrate_cpp(arma::Cube<int> pop) {
     if (m != 0) {
         if (model == "Step") {
-            for (i = 0; i < K; i++) {
-                for (j = 0; j < K; j++) {
-                    for(k = 1; k < (K - 1); k++) {
-                        i = RcppArmadillo::sample(perms, ceil(perms * m / 2), false);
-                        j = RcppArmadillo::sample(perms, ceil(perms * m / 2), false);
-                        tmp = pop[i, RcppArmadillo::sample(k, K, true)];
-                        pop[i, RcppArmadillo::sample(k, K, true)] = pop[j, RcppArmadillo::sample(k, K, true)];
-                        pop[j, RcppArmadillo::sample(k, K, true)] = tmp;
+            ind <- sample(perms, size = ceiling(perms * m/2), replace = FALSE)
+            for (i = 0; i < (K - 1); i++) {
+                        tmp = pop[ind, i];
+                        pop[ind, i] = pop[ind,, i + 1];
+                        pop[ind, i + 1] = tmp;
                     }
                 }
             }
-        }
-    }
     return pop;
 }
