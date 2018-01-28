96ab8abb8f0fd8d4d14521bed5a0d43926f68bad
Jarrett Phillips <jphill01@uoguelph.ca>
2018-01-27 20:18:41 -0500

    Add files via upload

--- migrate.cpp
+++ migrate.cpp
@@ -12,8 +12,8 @@ arma::Cube<int> migrate(arma::Cube<int> pop) {
     int K, perms;
     
     if (model == "Island") {
+        IntegerVector ind = RcppArmadillo::sample<IntegerVector>(perms, ceil(perms * m), true);
         for (int i = 0; i < (K - 1); i++) {
-            IntegerVector ind = RcppArmadillo::sample<IntegerVector>(perms, ceil(perms * m), true);
             int tmp = pop[ind, i];
             pop[ind, i] = pop[ind, i + 1];
             pop[ind, i + 1] = tmp;
