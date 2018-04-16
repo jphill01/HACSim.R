77ece97dd6c94ace97d4dafc10a1e456f893bf20
Jarrett Phillips <jphill01@uoguelph.ca>
2018-04-16 09:37:03 -0400

    Delete steff.R

--- steff.R
+++
@@ -1,9 +0,0 @@
-steff <- function() {
-  N1 <- HAC.sim(N = ceiling(N), Hstar = Hstar, probs = probs, K = K, perms = perms, p = p)
-  N2 <- HAC.sim(N = ceiling(Nstar), Hstar = Hstar, probs = probs, K = K, perms = perms, p = p)
-		while (R < p) {
-			N3 <- N - ((N1 - N)^2 / (N2 - 2 * N1 + N))
-			N <- N3
-			
-	} 
-}
