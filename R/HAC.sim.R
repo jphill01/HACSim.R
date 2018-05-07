7dc308a462103b44dbedfa895059966d42d5a764
Jarrett Phillips <jphill01@uoguelph.ca>
2018-05-06 21:23:48 -0400

    Add files via upload

--- R/HAC.sim.R
+++ R/HAC.sim.R
@@ -3,7 +3,7 @@
 ##########
 
 # Author: Jarrett D. Phillips
-# Last modified: April 30, 2018
+# Last modified: May 6, 2018
 
 ##########
 
@@ -19,6 +19,7 @@
 # probs = Probability frequency distribution of haplotypes
 # K = Number of (sub)populations (demes, sampling sites)
 # m = Overall migration rate between demes
+# mig.model = migration rate model - one of NULL, "Island", or "Step" (NULL by default)
 # perms = Number of permutations
 # p = Proportion of unique haplotypes to recover
 # input.seqs = Analyze inputted aligned/trimmed FASTA DNA sequence file (TRUE / FALSE)?
@@ -28,9 +29,10 @@
 
 HAC.sim <- function(N, 
                     Hstar, 
-                    probs, 
+                    probs,
                     K = 1,
                     m = 0,
+                    spatial = FALSE,
                     mig.model = NULL,
                     perms = 10000, 
                     p = 0.95,
@@ -99,45 +101,47 @@ HAC.sim <- function(N,
 	# individuals' haplotypes from the probabilities ##
 	
 	gen.perms <- function() {
-	    sample(haps, size = num.specs, replace = TRUE, prob = probs)
+	   sample(haps, size = num.specs, replace = TRUE, prob = probs)
 	}
 	
 	pop <- array(dim = c(perms, num.specs, K))
 	
 	  for (i in 1:K) {
 	    pop[,, i] <- replicate(perms, gen.perms())
-    }
+	  }
     
-    ## Allow individuals to migrate among subpopulations according to migration rate m ##
+  ## Allow individuals (columns) to migrate among subpopulations (levels) according to migration rate m ##
     
-    if (K > 1 && m != 0) {
+  if (K > 1 && m != 0) {
         
-        inds1 <- sample(perms, size = ceiling(perms * m), replace = FALSE)
-        inds2 <- sample(perms, size = ceiling(perms * m), replace = FALSE)
-        
-        # Linear Stepping Stone Model
-        
-        if (mig.model == "Step") {
-          for (i in 1:(K - 1) && 2:K) {
-            for(j in 1:(K - 1) && 2:K) {
-              tmp <- pop[inds1,, i]
-              pop[inds1,, i] <- pop[inds2,, j]
-              pop[inds2,, j] <- tmp
-            }
-          }
-        }
+    inds1 <- sample(num.specs, size = ceiling(num.specs * m), replace = FALSE)
+    inds2 <- sample(num.specs, size = ceiling(num.specs * m), replace = FALSE)
       
-      # Island Model
+    # Island Model
       
-      if (mig.model == "island") {
+      if (mig.model == "Island") {
         for (i in 1:K) {
           for(j in 1:K) {
-            tmp <- pop[inds1,, i]
-            pop[inds1,, i] <- pop[inds2,, j]
-            pop[inds2,, j] <- tmp
+            tmp <- pop[, inds1, i]
+            pop[, inds1, i] <- pop[, inds2, j]
+            pop[, inds2, j] <- tmp
           }
         }
-      } 
+      }
+    
+    # Linear Stepping Stone Model
+    
+    if (mig.model == "Step") {
+      for (i in 1:(K - 1) && 2:K) {
+        for(j in 1:(K - 1) && 2:K) {
+          tmp <- pop[, inds1, i]
+          pop[, inds1, i] <- pop[, inds2, j]
+          pop[, inds2, j] <- tmp
+        }
+      }
+    }
+    
+    # No model
         
      if (is.null(mig.model)) {
        pop
@@ -150,7 +154,7 @@ HAC.sim <- function(N,
 	
     HAC.mat <- accumulate(pop, specs, perms, K)
 
-	## Update progress bar ##
+  ## Update progress bar ##
 	
     if (progress == TRUE) {
       utils::setTxtProgressBar(pb, i)
@@ -169,13 +173,13 @@ HAC.sim <- function(N,
 	## Compute simple summary statistics and display output ##
 	
 	# tail() is used here instead of max() because curves will not be monotonic if perms is not set high enough. When perms is large (say 10000), tail() is sufficiently close to max()  
-	
-	P <- tail(means, n = 1)
-	Q <- Hstar - P
-	R <- P / HstarS
-	S <- (Hstar - P) / Hstar
-	Nstar <- (N * Hstar) / P
-	X <- ((N * Hstar) / P) - N
+	 
+	  P <- tail(means, n = 1)
+	  Q <- Hstar - P
+    assign("R", P / Hstar, envir = .GlobalEnv)
+	  S <- (Hstar - P) / Hstar
+	  assign("Nstar", (N * Hstar) / P, envir = .GlobalEnv)
+	  assign("X", ((N * Hstar) / P) - N, envir = .GlobalEnv)
 	
 	## Calculate slope of curve using last 10 points on curve
 	
@@ -186,15 +190,15 @@ HAC.sim <- function(N,
 	
     ## Output results to R console and text file ##
 		    
-	cat("\n \n --- Measures of Sampling Closeness --- \n \n", 
-	"Mean number of haplotypes sampled: " , P, 
-	"\n Mean number of haplotypes not sampled: " , Q, 
-	"\n Proportion of haplotypes (specimens) sampled: " , R, 
-	"\n Proportion of haplotypes (specimens) not sampled: " , S,
-	"\n \n Mean value of N*: ", Nstar / K, 
-	"\n Mean number of specimens not sampled: ", X / K, 
-	"\n \n Curve slope (last 10 points): ", b1,
-	"\n \n Mean number of specimens required to observe one new haplotype: ", 1 / b1, "\n \n")
+	  cat("\n \n --- Measures of Sampling Closeness --- \n \n", 
+	  "Mean number of haplotypes sampled: " , P, 
+	  "\n Mean number of haplotypes not sampled: " , Q, 
+	  "\n Proportion of haplotypes (specimens) sampled: " , R, 
+	  "\n Proportion of haplotypes (specimens) not sampled: " , S,
+	  "\n \n Mean value of N*: ", Nstar / K,
+	  "\n Mean number of specimens not sampled: ", X / K, 
+	  "\n \n Curve slope (last 10 points): ", b1,
+	  "\n \n Mean number of specimens required to observe one new haplotype: ", 1 / b1, "\n \n")
 	
     # name <- c("Mean number of haplotypes sampled", 
 	         # "Mean number of haplotypes not sampled", 
@@ -205,23 +209,23 @@ HAC.sim <- function(N,
 	         # "Mean value of N*", 
 	         # "Mean number of specimens not sampled")
 
-# write(P, file = "data.txt", append = TRUE)
-# write.table(c(P, Q, R, S, b1, 1 / b1, Nstar /K, X / K, file = "data.txt", append = TRUE)
-# write(Q, file = "Q.txt", append = TRUE)
-# write(R, file = "R.txt", append = TRUE)
-# write(S, file = "S.txt", append = TRUE)
-# write(b1, file = "b1.txt", append = TRUE)
-# write(1/b1, file = "invb1.txt", append = TRUE)
-# write(Nstar / K, file = "Nstar.txt", append = TRUE)
-# write(X / K, file = "X.txt", append = TRUE)
+  # write(P, file = "data.txt", append = TRUE)
+  # write.table(c(P, Q, R, S, b1, 1 / b1, Nstar /K, X / K, file = "data.txt", append = TRUE)
+  # write(Q, file = "Q.txt", append = TRUE)
+  # write(R, file = "R.txt", append = TRUE)
+  # write(S, file = "S.txt", append = TRUE)
+  # write(b1, file = "b1.txt", append = TRUE)
+  # write(1/b1, file = "invb1.txt", append = TRUE)
+  # write(Nstar / K, file = "Nstar.txt", append = TRUE)
+  # write(X / K, file = "X.txt", append = TRUE)
 	
 	## Plot the haplotype accumulation curve and haplotype frequency barplot ##
 
-			par(mfrow = c(1, 2))
-			plot(specs, means, type = "n", xlab = "Specimens sampled", ylab = "Unique haplotypes",  ylim = c(1, Hstar))
-			polygon(x = c(specs, rev(specs)), y = c(lower, rev(upper)), col = "gray")
-			lines(specs, means, lwd = 2)
-			HAC.bar <- barplot(num.specs * probs, xlab = "Unique haplotypes", ylab = "Specimens sampled", names.arg = 1:Hstar)
+	par(mfrow = c(1, 2))
+	plot(specs, means, type = "n", xlab = "Specimens sampled", ylab = "Unique haplotypes",  ylim = c(1, Hstar))
+	polygon(x = c(specs, rev(specs)), y = c(lower, rev(upper)), col = "gray")
+	lines(specs, means, lwd = 2)
+	HAC.bar <- barplot(num.specs * probs, xlab = "Unique haplotypes", ylab = "Specimens sampled", names.arg = haps)
             
 			
 }
