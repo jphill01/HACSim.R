851aa0e4a75e82aa607f9cf0e1a622e7fc05d532
Jarrett Phillips <jphill01@uoguelph.ca>
2018-05-29 14:23:52 -0400

    Add files via upload

---
+++ HAC.simrep.R
@@ -0,0 +1,61 @@
+### HAC Simulation Iteration ###
+
+## Run HAC Simulator until convergence (saturation) is reached ##
+
+HAC.simrep <- function(filename = "output") {
+  ptm <- proc.time()
+  iters <- 1
+  
+  df <- data.frame(matrix(ncol = 8, nrow = 0))
+  x <- c("Mean number of haplotypes sampled", 
+         "Mean number of haplotypes not sampled", 
+         "Proportion of haplotypes (specimens) sampled", 
+         "Proportion of haplotypes (specimens) not sampled",
+         "Mean value of N*", 
+         "Mean number of specimens not sampled",
+         "Haplotype accumulation curve slope", 
+         "Mean number of specimens required to observe one new haplotype")
+  colnames(df) <- x
+  
+  df <-HAC.sim(N = N, 
+               Hstar = Hstar, 
+               probs = probs, 
+               perms = perms, 
+               p = p,
+               subset.haps = subset.haps,
+               prop.haps = prop.haps,
+               input.seqs = input.seqs,
+               subset.seqs = subset.seqs,
+               prop.seqs = prop.seqs,
+               num.pts = 10,
+               prop.pts = NULL,
+               df = df
+  )
+  amt <- proc.time() - ptm
+  while (R < p) {
+    df <- HAC.sim(N = ceiling(Nstar), 
+                  Hstar = Hstar, 
+                  probs = probs,
+                  perms = perms,
+                  p = p,
+                  subset.haps = subset.haps,
+                  prop.haps = prop.haps,
+                  subset.seqs = subset.seqs,
+                  prop.seqs = prop.seqs,
+                  num.pts = 10,
+                  prop.pts = NULL,
+                  df = df
+    )
+    iters <- iters + 1
+    amt <- proc.time() - ptm
+  }
+  
+  ## Check whether desired level of haplotype recovery has been reached ##
+  if (R < p) {
+    cat("Desired level of H* has not yet been reached \n")
+  } else {
+    cat("\n Desired level of H* has been reached. \n \n The algorithm converged after", 
+        iters, "iterations and took", amt[3], "s.", "\n")
+  }
+  write.csv(df, file = paste(filename, ".csv", sep = ""))
+}
