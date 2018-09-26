d3f1649d8ff3cb0e3f9a67790d60c66be19a81a9
Jarrett Phillips <jphill01@uoguelph.ca>
2018-09-26 11:54:02 -0400

    Add files via upload

--- R/HAC.sim.R
+++ R/HAC.sim.R
@@ -177,7 +177,6 @@ HAC.sim <- function(N,
         }
     
       res <- matrix(replicate(num.seqs, duplicate.seq(res)), byrow = TRUE, nrow = num.seqs)
-      res <- res[sample(nrow(res), size = num.seqs, replace = TRUE), ]
       
       class(res) <- "DNAbin"
     
