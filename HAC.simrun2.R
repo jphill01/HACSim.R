3564105e2923c8dc0431c0667a6fc6ed522b303b
Jarrett Phillips <jphill01@uoguelph.ca>
2019-01-30 21:55:53 -0500

    Add files via upload

--- HAC.simrun2.R
+++ HAC.simrun2.R
@@ -36,33 +36,30 @@ library(HACSim)
 
 ## Set parameters for hypothetical species ##
 
-N <- 30 # total number of sampled individuals
-Hstar <- 10 # total number of haplotypes
-probs <- c(rep(0.45, 2), rep(0.10/8, 8)) # must sum to 1
+N <- 50 # total number of sampled individuals
+Hstar <- 20 # total number of haplotypes
+probs <- c(rep(0.30, 3), rep(0.10/17, 17)) # must sum to 1
 # probs <- rep(1/Hstar, Hstar) # equal haplotype frequency
 
 ### Run HAC Simulations ###
 
 ## Simulate hypothetical species ##
 ## If not set, prop defaults to 0.10
-HACSObj <- HACHypothetical(N, Hstar, probs, filename = "output")
+HACSObj <- HACHypothetical(N, Hstar, probs, conf.level = 0.99, conf.type = "asymptotic", filename = "output")
 
 ## Simulate hypothetical species - subsampling ##
-HACSObj <- HACHypothetical(N, Hstar, probs, subsample = TRUE, prop = 0.25, filename = "output")
+HACSObj <- HACHypothetical(N, Hstar, probs, conf.level = 0.99,  conf.type = "quantile", subsample = TRUE, prop = 0.25, filename = "output")
 
 ## Simulate hypothetical species and all paramaters changed - subsampling ##
-HACSObj <- HACHypothetical(N, Hstar, probs, perms = 10000, p = 0.90, subsample = TRUE, prop = 0.15, filename = "output")
+HACSObj <- HACHypothetical(N, Hstar, probs, perms = 10000, conf.level = 0.99, conf.type = "quantile", p = 0.90, subsample = TRUE, prop = 0.15, filename = "output")
 
 ## Simulate real species ##
-HACSObj <- HAC(type = "real", filename = "output")
+HACSObj <- HACReal(p = 0.95, perms = 10000, conf.level = 0.99, conf.type = "quantile", filename = "output")
 
 ## Simulate real species - subsampling ##
-HACSObj <- HAC(type = "real", subsample = TRUE, prop = 0.15, filename = "output")
+HACSObj <- HACReal(subsample = TRUE, prop = 0.15, onf.level = 0.99, conf.type = "quantile", filename = "output")
 
-## Simulate real species and all paramaters changed - subsampling ##
-HACSObj <- HAC(type = "real", perms = 10000, p = 0.90, subsample = TRUE, prop = 0.15, filename = "output")
-
-# Simulate DNA sequences under JC69
-HACSObj <- HAC(type = "evolution", subst.model = "JC69", num.seqs = 100, length.seqs = 500, mu.rate = 0.0010)
+## Simulate real species and all parameters changed - subsampling ##
+HACSObj <- HACReal(perms = 10000, p = 0.90, subsample = TRUE, prop = 0.15, onf.level = 0.99, conf.type = "quantile", filename = "output")
 
 HAC.simrep(HACSObj)
