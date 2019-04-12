0a011f350ffe1f5e592ce4c11f1cb85156685de5
Jarrett Phillips <jphill01@uoguelph.ca>
2019-04-12 12:00:50 -0400

    Add files via upload

--- R/zzz.R
+++ R/zzz.R
@@ -3,3 +3,7 @@ envr <- NULL
 .onLoad <- function(...) {
   envr <<- new.env()  # when package is loaded, create new environment to store needed variables 
 }
+
+.onAttach <- function(...) {
+  packageStartupMessage("This is HACSim 1.0.0")
+}
