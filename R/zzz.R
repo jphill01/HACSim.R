6af5e5991802b68cb28cbcc485237930d8dfed3c
Jarrett Phillips <jphill01@uoguelph.ca>
2019-04-13 01:00:57 -0400

    Add files via upload

--- R/zzz.R
+++ R/zzz.R
@@ -5,5 +5,8 @@ envr <- NULL
 }
 
 .onAttach <- function(...) {
-  packageStartupMessage("This is HACSim 1.0.0")
+  packageStartupMessage("This is HACSim 1.0.0 \n
+Type ?HACHypothetical to see how to set up objects to run \n simulations of haplotype accumulation for real species \n 
+Type ?HACReal to see how to set up objects to run \n simulations of haplotype accumulation for real species \n 
+Type ?HAC.sim to see how run simulations of haplotype \n accumulation curves")
 }
