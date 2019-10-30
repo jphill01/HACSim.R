902d49412c0755de0c68313e64b6aa43a6f3a465
Jarrett Phillips <jphill01@uoguelph.ca>
2019-10-29 22:03:47 -0400

    Add files via upload

--- R/HAC.simrep.R
+++ R/HAC.simrep.R
@@ -44,7 +44,7 @@ HAC.simrep <- function(HACSObject) {
                input.seqs = envr$input.seqs,
                conf.level = envr$conf.level,
                progress = envr$progress,
-               mum.iters = num.iters,
+               mum.iters = envr$num.iters,
                df = df
   )
   
