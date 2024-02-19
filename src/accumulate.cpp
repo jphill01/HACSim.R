b45cc58ed844152e7908a4de9a8b2de90fe16317
Jarrett Phillips <jphill01@uoguelph.ca>
2024-02-19 02:30:14 -0500

    Add files via upload

--- src/accumulate.cpp
+++ src/accumulate.cpp
@@ -1,6 +1,6 @@
 
 // [[Rcpp::depends(RcppArmadillo)]]
-// [[Rcpp::depends(RcppProgress)]]
+# // [[Rcpp::depends(RcppProgress)]]
 #define ARMA_DONT_PRINT_OPENMP_WARNING
 #include <RcppArmadillo.h>
 #include <RcppArmadilloExtensions/sample.h>
@@ -9,13 +9,13 @@ using namespace Rcpp;
 
 // Code used to cumulatively accumulate species' haplotypes
 // Randomly samples individuals (columns) from a given permutation (row) and enumerates
-// the total number of haplotypes found 
+// the total number of haplotypes found
 
 int sample_one(int n) {
   return n * unif_rand();
-} 
+}
 
-int sample_n_distinct(const IntegerVector& x, 
+int sample_n_distinct(const IntegerVector& x,
                       int k,
                       const int * pop_ptr) {
 
@@ -34,21 +34,24 @@ arma::Cube<int> accumulate(const arma::Cube<int>& pop,
                          const IntegerVector& specs,
                          int perms,
                          int K) {
-    
+
     int num_specs = specs.size();
     arma::Cube<int> res(perms, num_specs, K);
-    
+
     IntegerVector specs_C = specs - 1;
     const int * pop_ptr;
     int i, j, k;
-    
+
     for (i = 0; i < K; i++) {
-        for (k = 0; k < num_specs; k++) { 
-            for (j = 0; j < perms; j++) { 
-                pop_ptr = &(pop(sample_one(perms), 0, sample_one(K)));
+        for (k = 0; k < num_specs; k++) {
+            for (j = 0; j < perms; j++) {
+                // pop_ptr = &(pop(sample_one(perms), 0, sample_one(K)));
+                pop_ptr = &(pop(j, 0, sample_one(K)));
                 res(j, k, i) = sample_n_distinct(specs_C, k + 1, pop_ptr);
             }
         }
     }
     return res;
 }
+
+
